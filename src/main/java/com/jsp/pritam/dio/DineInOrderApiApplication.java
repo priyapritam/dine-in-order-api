bb9694833b244674700c7552781beeb519a9e484
priyapritam <priyapritam.pritam@gmail.com>
2025-02-19 16:09:01 +0530

    Initializing Project repo

---
+++ src/main/java/com/jsp/pritam/dio/DineInOrderApiApplication.java
@@ -0,0 +1,13 @@
+package com.jsp.pritam.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DineInOrderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DineInOrderApiApplication.class, args);
+	}
+
+}
